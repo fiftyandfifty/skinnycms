e7de81a1a3ca0768c9a2890900c77b85f13c36dc
RuslanHamidullin <ruslan.hamidullin@flatsoft.com>
2010-12-15 14:20:18 +0300

    [#7134431] FOR USERS MODULE: added basic admin/users controller, views/admin/users and layouts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   namespace :admin do
+    resources :users
     resources :pages
   end
 end
--- lib/generators/skinnycms_migrations/templates/devise_create_users.rb
+++ lib/generators/skinnycms_migrations/templates/devise_create_users.rb
@@ -1,6 +1,7 @@
 class DeviseCreateUsers < ActiveRecord::Migration
   def self.up
     create_table(:users) do |t|
+      t.string :name
       t.database_authenticatable :null => false
       t.confirmable
       t.recoverable
