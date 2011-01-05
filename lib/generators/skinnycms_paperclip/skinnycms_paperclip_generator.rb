2be986a5f0ca9bdfe4363146a16b32bfea0d94f3
Bryan Shanaver <bryan@shanaver.com>
2011-01-05 14:16:52 -0800

    include paperclip in skinny generator

--- lib/generators/skinnycms_paperclip/skinnycms_paperclip_generator.rb
+++ lib/generators/skinnycms_paperclip/skinnycms_paperclip_generator.rb
@@ -7,7 +7,7 @@ class SkinnycmsPaperclipGenerator < Rails::Generators::Base
   def copy_devise_file
     run "gem install paperclip -v '2.3.8'"
     inject_into_file "Gemfile", :before => "gem 'skinnycms'" do
-"gem 'paperclip', '2.3.8'\n\n"
+"gem 'paperclip', '2.3.8'\ngem 'cloudfiles', '1.4.10'\ngem 'paperclip-cloudfiles', '2.3.8', :require => 'paperclip'"
     end
     run "bundle install"
   end   
