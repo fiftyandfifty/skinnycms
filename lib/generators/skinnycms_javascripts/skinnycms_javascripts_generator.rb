ecb196c004df8567ce023be7e192c084ebaa6fe3
RuslanHamidullin <ruslan.hamidullin@flatsoft.com>
2011-01-24 20:40:31 +0300

    [#8938837] FOR ENGINE CONFIGURATION: fixed skinnycms_javascripts generator, deleted skinnycms_update generator

--- lib/generators/skinnycms_javascripts/skinnycms_javascripts_generator.rb
+++ lib/generators/skinnycms_javascripts/skinnycms_javascripts_generator.rb
@@ -11,13 +11,35 @@ class SkinnycmsJavascriptsGenerator < Rails::Generators::Base
   def copy_javascript_files
     puts SkinnycmsJavascriptsGenerator.start_description
     sleep(3)
-    copy_file "main.js", "public/javascripts/skinnycms/admin/main.js"
-    copy_file "rails.js", "public/javascripts/skinnycms/rails.js"
-    copy_file "nestedSortable.1.2.1/jquery-1.4.2.min.js", "public/javascripts/skinnycms/nestedSortable.1.2.1/jquery-1.4.2.min.js"
-    copy_file "nestedSortable.1.2.1/jquery-ui-1.8.2.custom.min.js", "public/javascripts/skinnycms/nestedSortable.1.2.1/jquery-ui-1.8.2.custom.min.js"
-    copy_file "nestedSortable.1.2.1/jquery.ui.nestedSortable.js", "public/javascripts/skinnycms/nestedSortable.1.2.1/jquery.ui.nestedSortable.js"
-    copy_file "jquery/jquery-1.4.4.min.js", "public/javascripts/skinnycms/jquery/jquery-1.4.4.min.js"
-    copy_file "jquery/jquery-ui-1.8.7.custom.min.js", "public/javascripts/skinnycms/jquery/jquery-ui-1.8.7.custom.min.js"
+
+    if !File.exist?("public/javascripts/skinnycms/admin/main.js")
+      copy_file "main.js", "public/javascripts/skinnycms/admin/main.js"
+    end
+
+    if !File.exist?("public/javascripts/skinnycms/rails.js")
+      copy_file "rails.js", "public/javascripts/skinnycms/rails.js"
+    end
+
+    if !File.exist?("public/javascripts/skinnycms/nestedSortable.1.2.1/jquery-1.4.2.min.js")
+      copy_file "nestedSortable.1.2.1/jquery-1.4.2.min.js", "public/javascripts/skinnycms/nestedSortable.1.2.1/jquery-1.4.2.min.js"
+    end
+
+    if !File.exist?("public/javascripts/skinnycms/nestedSortable.1.2.1/jquery-ui-1.8.2.custom.min.js")
+      copy_file "nestedSortable.1.2.1/jquery-ui-1.8.2.custom.min.js", "public/javascripts/skinnycms/nestedSortable.1.2.1/jquery-ui-1.8.2.custom.min.js"
+    end
+
+    if !File.exist?("public/javascripts/skinnycms/nestedSortable.1.2.1/jquery.ui.nestedSortable.js")
+      copy_file "nestedSortable.1.2.1/jquery.ui.nestedSortable.js", "public/javascripts/skinnycms/nestedSortable.1.2.1/jquery.ui.nestedSortable.js"
+    end
+
+    if !File.exist?("public/javascripts/skinnycms/jquery/jquery-1.4.4.min.js")
+      copy_file "jquery/jquery-1.4.4.min.js", "public/javascripts/skinnycms/jquery/jquery-1.4.4.min.js"
+    end
+
+    if !File.exist?("public/javascripts/skinnycms/jquery/jquery-ui-1.8.7.custom.min.js")
+      copy_file "jquery/jquery-ui-1.8.7.custom.min.js", "public/javascripts/skinnycms/jquery/jquery-ui-1.8.7.custom.min.js"
+    end
+    
     puts SkinnycmsJavascriptsGenerator.end_description
   end
 
