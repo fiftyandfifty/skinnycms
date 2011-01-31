005efba88edbf3f455387acf050ae4f4f602726b
RuslanHamidullin <ruslan.hamidullin@flatsoft.com>
2011-01-31 15:58:52 +0300

    FOR ASSETS MODULE: added sort by content_type in a views, edited disign

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,3 +1,11 @@
 class Asset < ActiveRecord::Base
   has_attached_file :asset
+
+  BASIC_TYPES = ['text', 'image', 'audio', 'video']
+
+  def content_type
+    BASIC_TYPES.each do |type|
+      return type if asset_content_type.include?(type)
+    end
+  end
 end
