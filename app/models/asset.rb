e655ddd8363fb22cb35918a86949e0503736f772
RuslanHamidullin <ruslan.hamidullin@flatsoft.com>
2011-02-28 18:22:39 +0300

    FOR ASSETS SECTION: implement preview for assets, deleted available modules from sidebar in new/edit assets views

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -10,11 +10,19 @@ class Asset < ActiveRecord::Base
                                 :bigthumb => "60x60>"
                                }
 
+  BASIC_TYPES = ['text', 'image', 'audio', 'video']
+
   before_asset_post_process :allow_only_images
 
   def allow_only_images
     if !(asset.content_type =~ %r{^(image|(x-)?application)/(x-png|pjpeg|jpeg|jpg|png|gif)$})
       return false
+    end
+  end
+
+  def content_type
+    BASIC_TYPES.each do |type|
+      return type if asset_content_type.include?(type)
     end
   end
 end
